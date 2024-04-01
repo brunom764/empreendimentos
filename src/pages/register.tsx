e9e2a026d30cec0addc7a1acf75925f426f7eb49
Bruno Miguel Moreira Albuquerque <brunom764@gmail.com>
2024-04-01 17:22:58 -0300

    feat: add header

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Container, Title } from './styles';
 import RegisterForm from '../components/forms/register';
+import Header from '../components/Header';
+import { useRouter } from 'next/dist/client/router';
 
 export default function RegisterEnterprise() {
-
+  const router = useRouter();
+  const goToHome = () => {
+    router.push('/');
+  }
   return (
-    <Container>
-      <Title>Cadastro de Empreendimento</Title>
-      <RegisterForm />
-    </Container>
+    <>
+        <Header 
+                  title="Cadastro de Empreendimento" 
+                  button={false} 
+                  IconReturn={true} 
+                  PushButton={() => {}}
+                  PushButtonReturn={goToHome}
+              />
+      <Container>
+        <RegisterForm />
+      </Container>
+    </>
   );
 }
