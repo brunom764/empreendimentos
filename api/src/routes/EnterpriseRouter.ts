cc0bf1749d2313dab984d2ff6331cf9d7c421f76
AlexandreDresch <connectWithAlexandre@proton.me>
2024-04-05 23:09:16 -0300

    feat: Add usage of SchemaValidator middleware in enterpriseRouter

--- api/src/routes/EnterpriseRouter.ts
+++ api/src/routes/EnterpriseRouter.ts
@@ -8,10 +8,44 @@ const enterpriseRouter: Router = Router();
 const enterpriseController = new EnterpriseController();
 const schemaValidator = new SchemaValidator();
 
-enterpriseRouter.post('/', (req, res) => enterpriseController.create(req, res));
-enterpriseRouter.get('/',  (req, res) => enterpriseController.getEnterprises(req, res));
-enterpriseRouter.get('/:id', (req, res) => enterpriseController.getEnterprise(req, res));
-enterpriseRouter.put('/:id',  (req, res) => enterpriseController.update(req, res));
-enterpriseRouter.delete('/:id', (req, res) => enterpriseController.delete(req, res));
+enterpriseRouter.post(
+  "/",
+  (req, res, next) => {
+    schemaValidator.validateBody(EnterpriseSchema.create.body, req, res, next)
+  }
+    ,
+  (req, res) => enterpriseController.create(req, res)
+);
+
+enterpriseRouter.get(
+  "/",
+  (req, res, next) =>
+    schemaValidator.validateQuery(EnterpriseSchema.list.query, req, res, next),
+  (req, res) => enterpriseController.getEnterprises(req, res)
+);
+
+enterpriseRouter.get('/:id', (req, res, next) => schemaValidator.validateParams(EnterpriseSchema.find.params, req, res, next), (req, res) => enterpriseController.getEnterprise(req, res));
+
+
+enterpriseRouter.put(
+  "/:id",
+  (req, res, next) => {
+    const paramsSchema = EnterpriseSchema.update.params;
+    const bodySchema = EnterpriseSchema.update.body;
+
+    schemaValidator.validateParams(paramsSchema, req, res, () => {
+      schemaValidator.validateBody(bodySchema, req, res, next);
+    });
+  },
+  (req, res) => enterpriseController.update(req, res)
+);
+
+enterpriseRouter.delete(
+    "/:id",    
+    (req, res, next) => {
+      schemaValidator.validateParams(EnterpriseSchema.delete.params, req, res, next);
+    },
+    (req, res) => enterpriseController.delete(req, res)
+  );
 
 export default enterpriseRouter;
