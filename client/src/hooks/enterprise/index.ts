e1c7c87a32662e37aab8768d0051e7afd18ced0f
AlexandreDresch <connectWithAlexandre@proton.me>
2024-04-06 16:03:08 -0300

    fix: Correct typo in the state name from 'setcurrentPage' to 'setCurrentPage' and update import in Home

--- client/src/hooks/enterprise/index.ts
+++ client/src/hooks/enterprise/index.ts
@@ -7,13 +7,13 @@ interface UseEnterpriseHook {
   currentPage: number;
   setEnterprises: React.Dispatch<React.SetStateAction<Enterprise[]>>;
   setSearchResults: React.Dispatch<React.SetStateAction<Enterprise[]>>;
-  setcurrentPage: React.Dispatch<React.SetStateAction<number>>;
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
 }
 
 const useEnterpriseHook = (initialEnterprises: any[]): UseEnterpriseHook => {
   const [enterprises, setEnterprises] = useState(initialEnterprises);
   const [searchResults, setSearchResults] = useState(initialEnterprises);
-  const [currentPage, setcurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
 
   return {
     enterprises,
@@ -21,7 +21,7 @@ const useEnterpriseHook = (initialEnterprises: any[]): UseEnterpriseHook => {
     currentPage,
     setEnterprises,
     setSearchResults,
-    setcurrentPage,
+    setCurrentPage,
   };
 };
 
